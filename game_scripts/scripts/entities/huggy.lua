7ef873961bdd4bd9d6430010615c54d0ea7895c2
Andrew Church <achurch@achurch.org>
2011-04-16 00:00:00 +0900

    Fix a potential crash when changing to fish form with huggies attached.
    This patch corrects a bug which can cause the game to crash when Naija
    changes to fish form while huggies are attached, because the script
    tries to use an already-freed bone pointer.  Another symptom is that the
    huggies suddenly disappear when Naija changes form (due to being warped
    to invalid coordinates), rather than flying off as usual.

--- game_scripts/scripts/entities/huggy.lua
+++ game_scripts/scripts/entities/huggy.lua
@@ -155,19 +155,22 @@ function update(me, dt)
 		end
 		
 		entity_exertHairForce(me, 0, 4, 1)				--tail gravity
-	
-		t = entity_getTarget(me)
-		entity_rotate(me, bone_getWorldRotation(attachBone))
-		entity_setPosition(me, bone_getWorldPosition(attachBone))
 		
+		local flyOff = false
 		if avatar_isRolling() then
 			rollTime = rollTime + dt
-			if rollTime > maxRollTime then			
-				entity_setState(me, STATE_FLYOFF, 0.5)
+			if rollTime > maxRollTime then
+				flyOff = true
 			end
 		end
 		if isForm(FORM_FISH) then
+			flyOff = true
+		end
+		if flyOff then  -- attachBone may no longer be valid!
 			entity_setState(me, STATE_FLYOFF, 0.5)
+		else
+			entity_rotate(me, bone_getWorldRotation(attachBone))
+			entity_setPosition(me, bone_getWorldPosition(attachBone))
 		end
 	end		
 	
@@ -214,6 +217,7 @@ function enterState(me)
 	elseif entity_isState(me, STATE_FLYOFF) then
 		entity_setEntityType(me, ET_ENEMY)
 		esetv(me, EV_LOOKAT,1)
+		attachBone = 0
 		rollTime = 0
 		entity_setMaxSpeed(me, 840)
 		entity_addRandomVel(me, 1200)
